a50d223e912f1911f67c31f15a6aa33c7d7c69ae
Jordanyoder2626 <120066655+Jordanyoder2626@users.noreply.github.com>
2026-06-29 11:15:20 -0400

    fixing all time standings + playoff apps (#12)

--- databases/updates/update_alltimestandings.py
+++ databases/updates/update_alltimestandings.py
@@ -1,14 +1,23 @@
-from scripts.records.initialize import *
+from scripts.records.initialize import get_all_time_standings
 from scripts.utils.database import Database
 from scripts.utils import constants
 
 
-season=constants.SEASON
+def _delete_all(table):
+    query = f'DELETE FROM {table};'
+    with Database() as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+
+
+season = constants.SEASON
 standings = get_all_time_standings(season)
 standings = standings.reset_index(drop=True).reset_index().rename(columns={'index': 'id'})
 
 records_table = 'alltime_standings'
 records_cols = constants.ALLTIME_STANDINGS_COLUMNS
+_delete_all(records_table)
 for idx, row in standings.iterrows():
     db = Database(table=records_table, columns=records_cols, values=tuple(row))
     db.sql_insert_query()
